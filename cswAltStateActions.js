806aa9d9e82adbce24d13c6714305687fefb9912
Christof Schwarz <christof.schwarz@databridge.ch>
2020-12-02 15:11:03 +0100

    Update cswAltStateActions.js

--- cswAltStateActions.js
+++ cswAltStateActions.js
@@ -1,43 +1,45 @@
 define(["jquery", "qlik"], function ($, qlik) {
     'use strict';
     //$("<style>").html(cssContent).appendTo("head");
+	
+	function copyStates(app, fromState, toState) {
+		app.clearAll(false, toState);  // clear selections in target State
+		app.getObject(null, 'CurrentSelections').then(function (ret) {
+			// get all selections
+			var allSel = ret.layout.selectionsInStates;
+			var fromStateSel;
+			// find the selections that belong to the toState 
+			allSel.forEach(function (sel) {
+				if (sel.stateName == fromState) fromStateSel = sel.qSelectionObject.qSelections;
+			});
+			
+			//console.log(fromStateSel);
+			fromStateSel.forEach(function (fieldDesc, i) {
+				//console.log('Selection found ',fieldDesc);
+                var fieldName = fieldDesc.name;
+                console.log('Selection found in field ' + fieldName);
+				var fieldObj = app.field(fieldName, toState);
+				fieldObj.clear();
+				var secretSauce = "[" + fieldName + "]=Aggr(Only({<[" + fieldName + "]=P([" + fromState + "]::[" + fieldName + "])>} [" + fieldName + "]), [" + fieldName + "])";
+				fieldObj.selectMatch("=" + secretSauce, true, true);
+				fieldObj.selectPossible();
+			});
 
-    function copyStates(app, fromState, toState) {
-        app.clearAll(false, toState);  // clear selections in target State
-        app.getObject(null, 'CurrentSelections').then(function (ret) {
-            // get all selections
-            var allSel = ret.layout.selectionsInStates;
-            var fromStateSel;
-            // find the selections that belong to the toState 
-            allSel.forEach(function (sel) {
-                if (sel.stateName == fromState) fromStateSel = sel.qSelectionObject.qSelections;
-            });
-
-            //console.log(fromStateSel);
-            fromStateSel.forEach(function (fieldDesc, i) {
-                console.log('Selection found in field ', fieldDesc.qField);
-                var fieldObj = app.field(fieldDesc.qField, toState);
-                fieldObj.clear();
-                var secretSauce = "[" + fieldDesc.qField + "]=Aggr(Only({<[" + fieldDesc.qField + "]=P(" + fromState + "::[" + fieldDesc.qField + "])>} [" + fieldDesc.qField + "]), [" + fieldDesc.qField + "])";
-                fieldObj.selectMatch("=" + secretSauce, true, true);
-                fieldObj.selectPossible();
-            });
-
-        });
-        return true;
-    }
-
+		});
+		return true;
+	}
+	
     return {
-
+		
         initialProperties: {
-            /* qListObjectDef: {
-                 qShowAlternatives: true,
-                 qFrequencyMode: "V",
-                 qInitialDataFetch: [{
-                     qWidth: 2,
-                     qHeight: 50
-                 }]
-             } */
+           /* qListObjectDef: {
+                qShowAlternatives: true,
+                qFrequencyMode: "V",
+                qInitialDataFetch: [{
+                    qWidth: 2,
+                    qHeight: 50
+                }]
+            } */
         },
         definition: {
 
@@ -70,30 +72,30 @@ define(["jquery", "qlik"], function ($, qlik) {
                                             })
                                         });
                                     }
-                                }, {
+                                },{
                                     type: "string",
                                     ref: "enterStyle",
                                     label: "Enter button style (CSS)",
                                     defaultValue: "margin:2px;display:block;width:200px;"
-                                }, {
+                                },{
                                     type: "string",
                                     ref: "labelCopyButton1",
                                     label: "Button Label: Copy Into Alt. State",
                                     defaultValue: "Copy Main \u25B6 Alt. State",
                                     expression: "optional"
-                                }, {
+                                },{
                                     type: "string",
                                     ref: "labelCopyButton2",
                                     label: "Button Label: Copy From Alt. State",
                                     defaultValue: "Copy Main \u25C0 Alt. State",
                                     expression: "optional"
-                                }, {
+                                },{
                                     type: "string",
                                     ref: "labelClearButton1",
                                     label: "Button Label: Clear Main State",
                                     defaultValue: "Clear Main State",
                                     expression: "optional"
-                                }, {
+                                },{
                                     type: "string",
                                     ref: "labelClearButton2",
                                     label: "Button Label: Clear Alt State",
@@ -106,13 +108,13 @@ define(["jquery", "qlik"], function ($, qlik) {
                 }
             }
         },
-
-        support: {
-            snapshot: false,
-            export: false,
-            exportData: false
+		
+        support : {
+          snapshot: false,
+          export: false,
+          exportData : false
         },
-
+		
         paint: function ($element, layout) {
 
             $element.css('overflow', 'auto');
@@ -121,7 +123,7 @@ define(["jquery", "qlik"], function ($, qlik) {
             var self = this;
             var ownId = this.options.id;
             var app = qlik.currApp(this);
-            //console.log('layout',layout);
+			//console.log('layout',layout);
             var html = '';
 
             if (layout.mystate == '') {
@@ -142,27 +144,27 @@ define(["jquery", "qlik"], function ($, qlik) {
                     html += '<button class="lui-button" style="' + layout.enterStyle + '" id="' + ownId + 'Clear1">';
                     html += layout.labelClearButton1 + '</button>';
                 }
-                if (layout.labelClearButton2.length > 0) {
+				if (layout.labelClearButton2.length > 0) {
                     html += '<button class="lui-button" style="' + layout.enterStyle + '" id="' + ownId + 'Clear2">';
                     html += layout.labelClearButton2 + '</button>';
                 }
                 $element.html(html);
 
-                $element.find("#" + ownId + 'Copy1').on("click", function () {
-                    copyStates(app, "$", layout.mystate);
-                });
-
-                $element.find("#" + ownId + 'Copy2').on("click", function () {
-                    copyStates(app, layout.mystate, "$");
-                });
-
-                $element.find("#" + ownId + 'Clear1').on("click", function () {
-                    app.clearAll(false, "$");
-                });
-
-                $element.find("#" + ownId + 'Clear2').on("click", function () {
-                    app.clearAll(false, layout.mystate);
-                });
+				$element.find("#"+ownId+'Copy1').on("click", function(){
+					copyStates(app, "$", layout.mystate);
+				});
+				
+				$element.find("#"+ownId+'Copy2').on("click", function(){
+					copyStates(app, layout.mystate, "$");
+				});
+				
+				$element.find("#"+ownId+'Clear1').on("click", function(){
+					app.clearAll(false, "$");
+				});
+				
+				$element.find("#"+ownId+'Clear2').on("click", function(){
+					app.clearAll(false, layout.mystate);
+				});
             }
         }
     };
