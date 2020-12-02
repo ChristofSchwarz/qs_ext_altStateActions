7030621a8b274c8265a441faf8dc2d44f0bbd03e
Christof Schwarz <christof.schwarz@databridge.ch>
2020-12-02 12:45:09 +0100

    Update cswAltStateActions.js

--- cswAltStateActions.js
+++ cswAltStateActions.js
@@ -1,139 +1,169 @@
-define(["jquery", "qlik", "text!./style.css"], function ($, qlik, cssContent) {
-	'use strict';
-	$("<style>").html(cssContent).appendTo("head");
-	return {
-		initialProperties: {
-			qListObjectDef: {
-				qShowAlternatives: true,
-				qFrequencyMode: "V",
-				qInitialDataFetch: [{
-					qWidth: 2,
-					qHeight: 50
-				}]
-			}
-		},
-		definition: {
-            
-			type: "items",
-			component: "accordion",
-			items: {
+define(["jquery", "qlik"], function ($, qlik) {
+    'use strict';
+    //$("<style>").html(cssContent).appendTo("head");
+
+    function copyStates(app, fromState, toState) {
+        app.clearAll(false, toState);  // clear selections in target State
+        app.getObject(null, 'CurrentSelections').then(function (ret) {
+            // get all selections
+            var allSel = ret.layout.selectionsInStates;
+            var fromStateSel;
+            // find the selections that belong to the toState 
+            allSel.forEach(function (sel) {
+                if (sel.stateName == fromState) fromStateSel = sel.qSelectionObject.qSelections;
+            });
+
+            //console.log(fromStateSel);
+            fromStateSel.forEach(function (fieldDesc, i) {
+                console.log('Selection found in field ', fieldDesc.qField);
+                var fieldObj = app.field(fieldDesc.qField, toState);
+                fieldObj.clear();
+                var secretSauce = "[" + fieldDesc.qField + "]=Aggr(Only({<[" + fieldDesc.qField + "]=P(" + fromState + "::[" + fieldDesc.qField + "])>} [" + fieldDesc.qField + "]), [" + fieldDesc.qField + "])";
+                fieldObj.selectMatch("=" + secretSauce, true, true);
+                fieldObj.selectPossible();
+            });
+
+        });
+        return true;
+    }
+
+    return {
+
+        initialProperties: {
+            /* qListObjectDef: {
+                 qShowAlternatives: true,
+                 qFrequencyMode: "V",
+                 qInitialDataFetch: [{
+                     qWidth: 2,
+                     qHeight: 50
+                 }]
+             } */
+        },
+        definition: {
+
+            type: "items",
+            component: "accordion",
+            items: {
                 settings: {
-					uses: "settings"
-				},
-				customProperties : {
+                    uses: "settings"
+                },
+                customProperties: {
                     component: "expandable-items",
                     label: "Custom Properties",
-                    type : "items",
-                    items : {
-                        state : {
-                            ref : "qListObjectDef.qStateName",
-                            label : "State",
-                            type : "string",
-                            component : "dropdown",
-                            defaultValue : "$",		
-                            options: function() {
-                                return qlik.currApp(this).getAppLayout().then(function (a){
-                                    return a.layout.qStateNames.map(function (state){
-                                          return {value : state, label : state}
-                                    })
-                                    /*
-                                    return [{value : "$", label : "Default"}].concat(layout.qStateNames.map(function (state){
-                                          return {value : state, label : state}
-                                    })); */
-                                });
-                            }
-                        },
-                        Layout : {
+                    type: "items",
+                    items: {
+                        Layout: {
                             type: "items",
-                            label: "Layout Settings",
-                            items: {
-                                enterStyle : {
+                            label: "Extension Settings",
+                            items: [
+                                {
+                                    ref: "mystate",
+                                    label: "Alternate State",
+                                    type: "string",
+                                    component: "dropdown",
+                                    //defaultValue : "$",		
+                                    options: function () {
+                                        var retArr = [];
+                                        return qlik.currApp().getAppLayout().then(function (a) {
+                                            return a.layout.qStateNames.map(function (state) {
+                                                return { value: state, label: state }
+                                            })
+                                        });
+                                    }
+                                }, {
                                     type: "string",
                                     ref: "enterStyle",
                                     label: "Enter button style (CSS)",
-                                    defaultValue: "color: #595959;padding: 0 4px;font-size: 13px;height: 28px;font-weight: bold;border-radius: 3px;border: solid 1px #cccccc;background-image: linear-gradient(to bottom, #ffffff, #e6e6e6);"
-                                }, 
-                                buttonLabel1 : {
+                                    defaultValue: "margin:2px;display:block;width:200px;"
+                                }, {
                                     type: "string",
-                                    ref: "labelCopyButton",
-                                    label: "Button Label: Copy State",
-                                    defaultValue: "Copy State"
-                                },
-                                buttonLabel2 : {
+                                    ref: "labelCopyButton1",
+                                    label: "Button Label: Copy Into Alt. State",
+                                    defaultValue: "Copy Main \u25B6 Alt. State",
+                                    expression: "optional"
+                                }, {
                                     type: "string",
-                                    ref: "labelClearButton",
-                                    label: "Button Label: Clear State",
-                                    defaultValue: "Clear State"
+                                    ref: "labelCopyButton2",
+                                    label: "Button Label: Copy From Alt. State",
+                                    defaultValue: "Copy Main \u25C0 Alt. State",
+                                    expression: "optional"
+                                }, {
+                                    type: "string",
+                                    ref: "labelClearButton1",
+                                    label: "Button Label: Clear Main State",
+                                    defaultValue: "Clear Main State",
+                                    expression: "optional"
+                                }, {
+                                    type: "string",
+                                    ref: "labelClearButton2",
+                                    label: "Button Label: Clear Alt State",
+                                    defaultValue: "Clear Alt. State",
+                                    expression: "optional"
                                 }
-                            }
+                            ]
                         }
                     }
                 }
-			}    
-		},
-		snapshot: {
-			canTakeSnapshot: false
-		},
-		paint: function ($element, layout) {
-		  
+            }
+        },
+
+        support: {
+            snapshot: false,
+            export: false,
+            exportData: false
+        },
+
+        paint: function ($element, layout) {
+
             $element.css('overflow', 'auto');
             //console.dir(layout);
-            
+
             var self = this;
-            var ownId = this.options.id;            
+            var ownId = this.options.id;
             var app = qlik.currApp(this);
-            var myState = layout.qListObject.qStateName;
+            //console.log('layout',layout);
             var html = '';
-		  	var currSel = app.getObject(null,'CurrentSelections');
-            //currSel.then(function(object){
-            //    console.log('Type: ' + object.layout.qInfo.qType);
-            //    console.log('Id: ' + object.layout.qInfo.qId);
-            //})
-            
-            if(myState === '$') {
-                
+
+            if (layout.mystate == '') {
+
                 html += '<p style="color:red;">Please select an alternate state in the settings.</p>';
                 $element.html(html);
-                
+
             } else {
-                
-                if(layout.labelCopyButton.length>0) {
-                    html += '<button style="' + layout.enterStyle + '" data-cmd="' + ownId + 'SelectField">';
-                    html += layout.labelCopyButton + '</button>';
-                    html += '&nbsp;'
+                if (layout.labelCopyButton1.length > 0) {
+                    html += '<button class="lui-button" style="' + layout.enterStyle + '" id="' + ownId + 'Copy1">';
+                    html += layout.labelCopyButton1 + '</button>';
                 }
-                if(layout.labelClearButton.length>0) {
-                    html += '<button style="' + layout.enterStyle + '" data-cmd="' + ownId + 'ClearAllState">';
-                    html += layout.labelClearButton + '</button>';
+                if (layout.labelCopyButton2.length > 0) {
+                    html += '<button class="lui-button" style="' + layout.enterStyle + '" id="' + ownId + 'Copy2">';
+                    html += layout.labelCopyButton2 + '</button>';
+                }
+                if (layout.labelClearButton1.length > 0) {
+                    html += '<button class="lui-button" style="' + layout.enterStyle + '" id="' + ownId + 'Clear1">';
+                    html += layout.labelClearButton1 + '</button>';
+                }
+                if (layout.labelClearButton2.length > 0) {
+                    html += '<button class="lui-button" style="' + layout.enterStyle + '" id="' + ownId + 'Clear2">';
+                    html += layout.labelClearButton2 + '</button>';
                 }
                 $element.html(html);
 
+                $element.find("#" + ownId + 'Copy1').on("click", function () {
+                    copyStates(app, "$", layout.mystate);
+                });
 
-                $element.find('button').on('qv-activate', function() {
-                    switch($(this).data('cmd')) {
+                $element.find("#" + ownId + 'Copy2').on("click", function () {
+                    copyStates(app, layout.mystate, "$");
+                });
 
-                    case ownId + 'SelectField':
-                        //clear selections in target State
-                        app.clearAll(false,myState);    
-                        //copy all selections from the main selection in a loop over the fields
-                        currSel.then(function(object){
-                            $.each(object.layout.qSelectionObject.qSelections, function(key, value){
-                                console.log('Selection found in field ' + value.qField);
-                                var myField = app.field(value.qField, myState);  
-                                myField.clear();
-                                var secretSauce = "["+value.qField+"]=Aggr(Only({<["+value.qField+"]=P($::["+value.qField+"])>} ["+value.qField+"]), ["+value.qField+"])";
-                                myField.selectMatch("=" + secretSauce, true, true); 
-                                myField.selectPossible();                      
-                            });
-                        });
-                        break;
+                $element.find("#" + ownId + 'Clear1').on("click", function () {
+                    app.clearAll(false, "$");
+                });
 
-                    case ownId + 'ClearAllState':
-                        app.clearAll(false,myState);    
-                        break;
-                    }
+                $element.find("#" + ownId + 'Clear2').on("click", function () {
+                    app.clearAll(false, layout.mystate);
                 });
             }
-		}
-	};
-} );
+        }
+    };
+});
